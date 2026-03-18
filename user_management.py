caf5e1914de1694dfb95ebeba85fbc800d29699a
HarrrrY145 <harry.burton1@education.nsw.gov.au>
2026-03-19 00:02:25 +1100

    19/3/26 COMMIT 3 | Adding parameters when selecting usernames and passwords from the 'user' database in the retrieveUsers Function.

--- user_management.py
+++ user_management.py
@@ -6,32 +6,36 @@ import random
 def insertUser(username, password, DoB):
     con = sql.connect("database_files/database.db")
     cur = con.cursor()
-    cur.execute(
 
-#CHECK FOR SQL INJECTION
-        "INSERT INTO users (username,password,dateOfBirth) VALUES (?,?,?)",
-        (username, password, DoB),)
+
+#This is SQL injection proof. 
+    cur.execute("INSERT INTO users (username,password,dateOfBirth) VALUES (?,?,?)", (username, password, DoB),)
     con.commit()
     con.close()
 
 #CHECK FOR FILE SAFTEY 
 def retrieveUsers(username, password):
-    con = sql.connect("database_files/database.db")
-    cur = con.cursor()
-    cur.execute(f"SELECT * FROM users WHERE username = '{username}'")
-    if cur.fetchone() == None:
-        con.close()
-        return False
-    else:
-        cur.execute(f"SELECT * FROM users WHERE password = '{password}'")
-        # Plain text log of visitor count as requested by Unsecure PWA management
-        with open("visitor_log.txt", "r") as file:
-            number = int(file.read().strip())
-            number += 1
-        with open("visitor_log.txt", "w") as file:
-            file.write(str(number))
+    con = sql.connect("database_files/database.db") #Connecting to database
+    cur = con.cursor() 
+
+    cur.execute("SELECT * FROM users WHERE username == ?", (username)) # Selecting the username from database
+    if cur.fetchone() == None: # If no username is found in the database
+        con.close() # Close database
+        return False 
+
+    else: # If a username is found in the database
+        cur.execute("SELECT * FROM users WHERE password == ?", (password)) # Select the password from the database
+#------------------------------------------------------------------------
+# Plain text log of visitor count as requested by Unsecure PWA management
+#------------------------------------------------------------------------
+        with open("visitor_log.txt", "r") as file: # Opening visitor log as 'read'
+            number = int(file.read().strip()) # Reading the file strip and saving it as an intiger number vairable 
+            number += 1 # Plus the read number by 1 
+        with open("visitor_log.txt", "w") as file: # Opening visitor log as 'write'
+            file.write(str(number)) # write the new number into the file. 
+
         # Simulate response time of heavy app for testing purposes
-        time.sleep(random.randint(80, 90) / 1000)
+        time.sleep(random.randint(80, 90) / 1000) 
         if cur.fetchone() == None:
             con.close()
             return False
@@ -41,23 +45,24 @@ def retrieveUsers(username, password):
 
 # Inserting feedback into a database
 def insertFeedback(feedback):
-    con = sql.connect("database_files/database.db")
-    cur = con.cursor()
+    con = sql.connect("database_files/database.db") # Connecting to database 
+    cur = con.cursor() 
 
 #CHECK FOR SQL INJECTION
-    cur.execute("INSERT INTO feedback (feedback) VALUES (?)", (feedback,)) # Removing the F-string and implementimg query parameters to prevent SQL injections.
-    con.commit()
-    con.close()
+# Removing the F-string and implementimg query parameters to prevent SQL injections.
+    cur.execute("INSERT INTO feedback (feedback) VALUES (?)", (feedback,)) # Inserting feedback messages into the database
+    con.commit() # Commiting to database
+    con.close() # Closing database
 
 #CHECK FOR FILE SAFTEY 
 def listFeedback():
-    con = sql.connect("database_files/database.db")
-    cur = con.cursor()
-    data = cur.execute("SELECT * FROM feedback").fetchall()
-    con.close()
-    f = open("templates/partials/success_feedback.html", "w")
-    for row in data:
-        f.write("<p>\n")
-        f.write(f"{row[1]}\n")
-        f.write("</p>\n")
-    f.close()
+    con = sql.connect("database_files/database.db") # Connecting to database
+    cur = con.cursor() 
+    data = cur.execute("SELECT * FROM feedback").fetchall() # Selecting all feedback from database
+    con.close() # Close database
+    f = open("templates/partials/success_feedback.html", "w") # Open the success feedback html file as 'write' 
+    for row in data: # For every row 
+        f.write("<p>\n") # New line
+        f.write(f"{row[1]}\n") # Write feedback
+        f.write("</p>\n") # New line
+    f.close() # Close file
